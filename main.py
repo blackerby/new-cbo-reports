290ac64ad7dbc308a5f5467d43366538e7fc9f20
William Blackerby <wmblackerby@gmail.com>
2026-08-19 09:53:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+from enum import StrEnum, auto
 from datetime import date, timedelta
 import os
-import re
 
 import duckdb
 import polars as pl
@@ -10,15 +10,27 @@ import streamlit as st
 CURRENT_CONGRESS = str((date.today().year - 1789) // 2 + 1)
 CBO_URL = f"https://www.cbo.gov/rss/{CURRENT_CONGRESS}congress-cost-estimates.xml"
 CDG_API_URL = "https://api.congress.gov/v3"
+CDG_URL = "https://www.congress.gov"
 YESTERDAY = date.today() - timedelta(days=1)
 API_KEY = os.getenv("CDG_API_KEY", "DEMO_KEY")
 
 
-def url_for(bill_number):
+class Host(StrEnum):
+    API = auto()
+    WEB = auto()
+
+
+def url_for(bill_number, host):
     bill_type, number = [
         s.replace(".", "").replace(" ", "").lower() for s in bill_number.rsplit(".", 1)
     ]
-    return f"{CDG_API_URL}/bill/{CURRENT_CONGRESS}/{bill_type}/{number}"
+
+    if host == Host.WEB:
+        url = CDG_URL
+    if host == Host.API:
+        url = CDG_API_URL
+
+    return f"{url}/bill/{CURRENT_CONGRESS}/{bill_type}/{number}"
 
 
 def create_duckdb_con():
@@ -63,11 +75,16 @@ def get_df():
         .filter(pl.col("Date").dt.date() == YESTERDAY)
         .with_columns(
             pl.col("Bill_Number")
-            .map_elements(url_for, return_dtype=pl.String)
+            .map_elements(lambda n: url_for(n, Host.WEB), return_dtype=pl.String)
             .alias("Bill URL")
         )
         .with_columns(
-            pl.col("Bill URL")
+            pl.col("Bill_Number")
+            .map_elements(lambda n: url_for(n, Host.API), return_dtype=pl.String)
+            .alias("Bill API URL")
+        )
+        .with_columns(
+            pl.col("Bill API URL")
             .map_elements(fetch_cdg_data, return_dtype=pl.String)
             .alias("cdg_api_cbo_url")
         )
@@ -76,7 +93,7 @@ def get_df():
                 "Cost Estimate Present on CDG?"
             )
         )
-        .select("Date", "Title", "Bill URL", "Cost Estimate Present on CDG?")
+        .select("Date", "Title", "Bill URL", "Bill API URL", "Cost Estimate Present on CDG?")
     )
 
     return df
@@ -93,6 +110,7 @@ st.dataframe(
     column_config={
         "Link": st.column_config.LinkColumn("Link"),
         "Bill URL": st.column_config.LinkColumn("Bill URL"),
+        "Bill API URL": st.column_config.LinkColumn("Bill API URL"),
     },
 )
 
